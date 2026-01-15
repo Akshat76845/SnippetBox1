38457dee3536e17030f1ed60c47ec8d854cfdc62
Akshat Gupta <akshatgupta78886@gmail.com>
2026-01-16 05:09:55 +0530

    Clean URL and Method Based Routing

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -1,34 +1,64 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
+	"snippetbox.alexedwards.net/internal/models"
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w,r)
+
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w,err)
 		return 
 	}
-	w.Write([]byte("Hello from Snippetbox"))
+
+	data := app.newTemplateData(r)
+	data.snippets = snippets
+	app.render(w, http.StatusOK, "home.tmpl", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w,r)
+		app.NotFound(w)
 		return 
 	}
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		}
+		else{
+			app.serverError(w,err)
+		}
+		return
+	}
+	data := app.newTemplateData(r)
+	data.snippet = snippet
+	app.render(w, http.StatusOK, "view.tmpl", data)
 }
 
 
-func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return 
-	}
-	w.Write([]byte("Create a new snippet..."))
+func (app *application) snippetCreatePost(w http.ResponseWriter, r
+*http.Request) {
+// Checking if the request method is a POST is now superfluous and can be
+// removed, because this is done automatically by httprouter.
+title := "O snail"
+content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n–
+Kobayashi Issa"
+expires := 7
+id, err := app.snippets.Insert(title, content, expires)
+if err != nil {
+app.serverError(w, err)
+return
+}
+// Update the redirect path to use the new clean URL format.
+http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id),
+http.StatusSeeOther)
 }
--- snippetbox/cmd/web/routes.go
+++ snippetbox/cmd/web/routes.go
@@ -1,13 +1,26 @@
 package main
-import "net/http"
+import (
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
+)
 
-func (app *application) routes() *http.ServeMux {
-	  mux := http.NewServeMux()
-	  fileServer := http.FileServer(http.Dir("./ui/static/"))
-	  mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	  mux.HandleFunc("/", app.home)
-	  mux.HandleFunc("/snippet/view", app.snippetView)
-	  mux.HandleFunc("/snippet/create", app.snippetCreate)
+func (app *application) routes() http.Handler{
+	// Initialize the router.
+	router := httprouter.New()
+	// Update the patter for the route for the static files.
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	router.Handler(http.MethodGet, "/static/*filepath",
+		http.StripPrefix("/static", fileServer))
 
-	  return mux
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
+
+
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	return standard.Then(router)
+
+
 }
